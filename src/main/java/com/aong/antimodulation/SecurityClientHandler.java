281b8f0cb1768db49190d2031ebdd42daad1285c
aong494 <130423413+aong494@users.noreply.github.com>
2026-04-06 19:06:48 +0900

    Update SecurityClientHandler.java

--- src/main/java/com/aong/antimodulation/SecurityClientHandler.java
+++ src/main/java/com/aong/antimodulation/SecurityClientHandler.java
@@ -15,13 +15,11 @@ public class SecurityClientHandler {
         Screen screen = event.getScreen();
         String name = screen.getClass().getSimpleName();
 
-        // 서버 관련 창이 열릴 때
         if (name.contains("EditServer") || name.contains("Direct")) {
             int count = 0;
             for (var child : screen.children()) {
                 if (child instanceof EditBox editBox) {
                     count++;
-                    // 주소창(2번째 박스 또는 직접연결 1번째)을 찾으면
                     if ((name.contains("Direct") && count == 1) || count == 2) {
                     }
                 }
